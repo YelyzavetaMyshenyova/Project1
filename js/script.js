72ff1ed80fdf47f082913eb1b7cba960ba50abf0
YelyzavetaMyshenyova <46732280+YelyzavetaMyshenyova@users.noreply.github.com>
2019-04-30 22:38:56 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -23,13 +23,15 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("slides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+      slides[i].style.display = "none";
   }
-
+ 
   slides[slideIndex-1].style.display = "block";
 
-}
 
+}
