8ca754f2ad123a25fd7afc269b68585cf68b0d58
YelyzavetaMyshenyova <46732280+YelyzavetaMyshenyova@users.noreply.github.com>
2019-04-30 22:20:17 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,20 +8,28 @@ function initMap() {
 }
 
 window.addEventListener('load', initMap);
+
 var slideIndex = 1;
-showDivs(slideIndex);
+showSlides(slideIndex);
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-function showDivs(n) {
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
   var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = x.length} ;
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none"; 
+  var slides = document.getElementsByClassName("slides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
-  x[slideIndex-1].style.display = "block"; 
+
+  slides[slideIndex-1].style.display = "block";
+
 }
+
